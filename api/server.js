3e5b96ced0668de8d7c900003b76539b43fc7eb1
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-08-31 16:50:42 -0700

    built server.

--- api/server.js
+++ api/server.js
@@ -1,2 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: '404 Error. Not Found.'
+  })
+})
+
+module.exports = server;
